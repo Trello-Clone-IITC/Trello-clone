8b42a494a4a0663d59f16a1bbd6603b6c45d7950
niveh115 <nivcaspi3@gmail.com>
2025-09-02 19:03:19 +0300

    added navbar (#12)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import TestCard from "./features/Board/components/card/components/TestCard";
+import { Navbar } from "./features/Navbar";
 
 function App() {
   return (
-    <div className="bg-[#161a1d] min-h-screen w-full">
-      <div className="p-8">
-        <h1 className="text-2xl font-bold mb-6 text-white">
-          Trello Card Modal Test
-        </h1>
-        <div className="max-w-sm">
-          <TestCard />
+    <div className="min-h-screen w-full">
+      <Navbar />
+      <div className="bg-[#161a1d] min-h-screen w-full">
+        <div className="p-8">
+          <h1 className="text-2xl font-bold mb-6 text-white">
+            Trello Card Modal Test
+          </h1>
+          <div className="max-w-sm">
+            <TestCard />
+          </div>
         </div>
       </div>
     </div>
