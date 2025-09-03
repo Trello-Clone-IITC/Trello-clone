746da32a27fe08f3617b6ffa5ee0333afc7b630d
aimanshomre <aimanshomre@gmail.com>
2025-09-03 22:43:34 +0300

    changeg routes and card controller

--- server/src/api/cards/index.ts
+++ server/src/api/cards/index.ts
@@ -1,16 +1,16 @@
-import { Router } from "express";
-import cardRoutes from "./card.route.js";
-// import checklistRoutes from "../checklists/index.js";
-// import commentRoutes from "../comments/index.js";
-// import attachmentRoutes from "../attachments/index.js";
-// import labelRoutes from "../labels/index.js";
+// import { Router } from "express";
+// import cardRoutes from "./card.route.js";
+// // import checklistRoutes from "../checklists/index.js";
+// // import commentRoutes from "../comments/index.js";
+// // import attachmentRoutes from "../attachments/index.js";
+// // import labelRoutes from "../labels/index.js";
 
-const router = Router();
+// const router = Router();
 
-router.use("/", cardRoutes);
-// router.use("/", checklistRoutes);
-// router.use("/", commentRoutes);
-// router.use("/", attachmentRoutes);
-// router.use("/", labelRoutes);
+// router.use("/", cardRoutes);
+// // router.use("/", checklistRoutes);
+// // router.use("/", commentRoutes);
+// // router.use("/", attachmentRoutes);
+// // router.use("/", labelRoutes);
 
-export default router;
+// export default router;
